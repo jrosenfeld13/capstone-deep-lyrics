aaea6fd7d9fb746c6cbbd887170e5779b999aae6
Jason Rosenfeld <j.rosen.1392@gmail.com>
2018-09-28 12:30:00 -0400

    CLI improvements

--- src/nlp/generate_text.py
+++ src/nlp/generate_text.py
@@ -1,8 +1,14 @@
 import trigram
-    
-def main():
-    lm = trigram.SimpleTrigramLM(words=None, probas_file='../../data/models/trigram-weights')
+import argparse
+
+def main(infile):
+    #instantiate a trigram model with pretrained weights and generate text
+    lm = trigram.SimpleTrigramLM(words=None, infile=infile)
     lm.generate_text(max_length=500)
     
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--infile', type=str, default='../../data/models/trigram-weights.pkl')
+    args = parser.parse_args()
+    
+    main(infile=args.infile)
