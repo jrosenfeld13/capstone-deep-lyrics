e42bb1d1e41d1c233c20e542fb547653e170ba50
steveyang90 <steven.s.yang@gmail.com>
2018-10-08 11:23:22 -0700

    cluster provisioning script

--- src/data/start-dataproc-cluster.sh
+++ src/data/start-dataproc-cluster.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
-gcloud dataproc clusters create w210-capstone\
-  --metadata "JUPYTER_CONDA_PACKAGES=pandas:tqdm:beautifulsoup4:python-dotenv" \
+# number of clusters
+if [ "$1" != "" ]; then
+    WORKERS=$1
+else
+    WORKERS=2
+fi
+
+gcloud dataproc clusters create w210-capstone \
+  --metadata "JUPYTER_CONDA_CHANNELS=conda-forge,JUPYTER_CONDA_PACKAGES=pandas:tqdm:beautifulsoup4:python-dotenv" \
   --bucket w210-capstone \
   --subnet default \
   --zone us-central1-a \
   --master-machine-type n1-standard-1 \
   --master-boot-disk-size 80 \
-  --num-workers 2 \
+  --num-workers $WORKERS \
   --worker-machine-type n1-standard-1 \
   --worker-boot-disk-size 80 \
   --image-version 1.2 \
