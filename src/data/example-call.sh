36fcac5500c8ea4252d77f882912ddbcb58de48c
steveyang90 <steven.s.yang@gmail.com>
2018-10-09 22:40:10 -0700

    example call

--- src/data/example-call.sh
+++ src/data/example-call.sh
@@ -5,6 +5,9 @@
 #   done
 # done
 
-bash start-dataproc-cluster.sh -w 12 -g us-west2-c; \
-bash start-dataproc-job.sh gs:///genius-meta/metadata_part_at; \
+bash start-dataproc-cluster.sh -w 12 -g us-central1-f; \
+bash start-dataproc-job.sh gs:///genius-meta/metadata_part_ag; \
 gcloud dataproc clusters delete w210-capstone --quiet
+
+# verify what is causing failure in metadata_part_as
+# is it the IP of a worker? Or is it a specific request that keeps failing?
