24582413ed2bd77ecbdf47c9663444939417338e
steveyang90 <steven.s.yang@gmail.com>
2018-12-07 04:58:20 +0000

    revert hacky changes

--- src/web/app.js
+++ src/web/app.js
@@ -2,4 +2,4 @@ var express = require('express'); // Express web server framework
 var app = express();
 app.use(express.static(__dirname));
 console.log('Listening on 8888');
-app.listen(8889);
+app.listen(8888);
