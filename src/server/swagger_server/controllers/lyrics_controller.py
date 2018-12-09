7dd23b162d401e8ace9f2a851b38a27bdf8e3708
jrosenfeld13 <j.rosen.1392@gmail.com>
2018-12-09 18:55:37 +0000

    generate/controller working towards multimodal

--- src/server/swagger_server/controllers/lyrics_controller.py
+++ src/server/swagger_server/controllers/lyrics_controller.py
@@ -24,33 +24,48 @@ def generate_lyrics(body):  # noqa: E501
     if connexion.request.is_json:
         body = connexion.request.get_json()  # noqa: E501
 
+    ###ESTABLISH MODEL TYPE
+    body['model_type'] = 'language'
     MODEL = '4.2-LM-108k-lines-genre-song_title'
 
-    ### LOAD MODEL AND CREATE DEEPLYRIC OBJECT
-    #load from google cloudstore
-    #deep_lyric = DeepLyric(MODEL, model_type='language', model_name=MODEL)
+    if body['model_type'] == 'multimodal':
+        MODEL = '4.3-MM-108k-post-genre-song_title'
     
-    #load from local (needs relative path workaround)
+    ### LOAD MODEL AND CREATE DEEPLYRIC OBJECT  
+    #load from local (needs this relative path workaround)
     ARCHITCTURE_FILE = os.path.join(os.path.dirname(__file__), f'../../../../data/models/{MODEL}/models/{MODEL}_architecture.pkl')
     ITOS_FILE = os.path.join(os.path.dirname(__file__), f'../../../../data/models/{MODEL}/tmp/itos.pkl')
+    PREPROCESSOR_FILE = os.path.join(os.path.dirname(__file__), f'../../../../data/models/{MODEL}/{MODEL}_preprocessor.pkl')
 
-    #MODEL_PATH = Path(f'../../../../data/models/{MODEL}')
+    
     with open(ARCHITCTURE_FILE, 'rb') as f:
         architecture = pickle.load(f)
     with open(ITOS_FILE, 'rb') as f:
         itos = pickle.load(f)
-    deep_lyric = DeepLyric(architecture, itos, model_type='language')
+    
+    preprocessor=None
+    if body['model_type'] == 'multimodal':
+        with open(PREPROCESSOR_FILE, 'rb') as f:
+            preprocessor = pickle.load(f)
+        
+    deep_lyric = DeepLyric(architecture, itos, model_type=body['model_type'], preprocessor=preprocessor)
     
     ### PARSE PARAMS FROM WEB
     #genre
     genre = body['genres']
     if len(genre) > 1:
-        genre = genre[0] # first for now      
+        genre = genre[0]      
     
     #title
     title = body['title']
     
     deep_lyric.set_config('seed_text', f'xbos xgenre {genre} xtitle {title}')
+    deep_lyric.set_config('max_len', 130)
+    deep_lyric.set_config('context_length', 70)
+    deep_lyric.set_config('beam_width', 3)
+    deep_lyric.set_config('top_k', 5)
+    deep_lyric.set_config('temperature', 1.45)
+    
     deep_lyric.generate_text()
     
     #out = deep_lyric.save_json(out=True)
